037462522f83df26278620a660284925df153db5
Jennifer Chan <jenchanws@gmail.com>
2023-05-23 01:47:12 -0400

    Move exit data to NBT, simplify blockstates, and add exit renderer

--- common/src/main/java/com/railwayteam/railways/mixin/client/MixinGlobalRailwayManager.java
+++ common/src/main/java/com/railwayteam/railways/mixin/client/MixinGlobalRailwayManager.java
@@ -1,9 +1,14 @@
 package com.railwayteam.railways.mixin.client;
 
+import com.railwayteam.railways.content.switches.TrackSwitch;
+import com.railwayteam.railways.content.switches.TrackSwitchDebugVisualizer;
 import com.railwayteam.railways.content.train_debug.TravellingPointVisualizer;
+import com.railwayteam.railways.registry.CREdgePointTypes;
 import com.simibubi.create.content.contraptions.KineticDebugger;
 import com.simibubi.create.content.logistics.trains.GlobalRailwayManager;
+import com.simibubi.create.content.logistics.trains.TrackGraph;
 import com.simibubi.create.content.logistics.trains.entity.Train;
+import com.simibubi.create.content.logistics.trains.management.edgePoint.EdgePointType;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
@@ -12,6 +17,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import java.util.Map;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 @Mixin(value = GlobalRailwayManager.class, remap = false)
 public abstract class MixinGlobalRailwayManager {
@@ -23,4 +29,17 @@ public abstract class MixinGlobalRailwayManager {
             for (Train train : trains.values())
                 TravellingPointVisualizer.debugTrain(train);
     }*/
+
+    @Shadow public Map<UUID, TrackGraph> trackNetworks;
+
+    @Inject(method = "clientTick", at = @At("HEAD"))
+    private void showSwitchDebug(CallbackInfo ci) {
+        if (KineticDebugger.isF3DebugModeActive()) {
+            for (TrackGraph graph : trackNetworks.values()) {
+                for (TrackSwitch sw: graph.getPoints(CREdgePointTypes.SWITCH)) {
+                    TrackSwitchDebugVisualizer.visualizeSwitchExits(sw);
+                }
+            }
+        }
+    }
 }
